d2be56bf5dea5de42fd0cda7589ae713496a5c6c
Monica6788 <kekelee0070@gmail.com>
2026-06-22 18:01:49 +0900

    상속(적용 전, 후로 나누어 학습)

---
+++ 08_Inherit/src/com/kh/after/PaymentMethod.java
@@ -0,0 +1,41 @@
+package com.kh.after;
+
+// 공통되는 결제 정보를 관리하기 위한 클래스
+// payName, payAmount, 결제정보 출력(내용은 다르지만 public void 매개변수 없음)
+public class PaymentMethod {
+	// 모든 클래스는 부모 클래스가 존재!!!
+	// 모든 클래스는 Object 클래스의 자식 클래스
+	private String payName;
+	private int payAmount;
+	
+	public PaymentMethod() {
+		super();
+	}
+	
+	public PaymentMethod(String payName, int payAmount) {
+		super();
+		this.payName = payName;
+		this.payAmount = payAmount;
+	}
+
+	public String getPayName() {
+		return payName;
+	}
+	public void setPayName(String payName) {
+		this.payName = payName;
+	}
+
+	public int getPayAmount() {
+		return payAmount;
+	}
+	public void setPayAmount(int payAmount) {
+		this.payAmount = payAmount;
+	}
+		
+	public void processPay() {
+		System.out.println("========== 결제 정보 ==========");
+		System.out.println("- 결제명: " + payName);
+		System.out.println("- 결제 금액: " + payAmount);
+	}
+
+}
---
+++ 08_Inherit/src/com/kh/before/PointPay.java
@@ -0,0 +1,60 @@
+package com.kh.before;
+
+public class PointPay {
+	private String payName;		// 결제명
+	private int payAmount;		// 결제 금액
+	private int myPoint;		// 포인트 점수
+	
+	
+	public PointPay(String payName, int payAmount, int myPoint) {
+		this.payName = payName;
+		this.payAmount = payAmount;
+		this.myPoint = myPoint;
+	}
+
+
+	public String getPayName() {
+		return payName;
+	}
+	public void setPayName(String payName) {
+		this.payName = payName;
+	}
+
+	public int getPayAmount() {
+		return payAmount;
+	}
+	public void setPayAmount(int payAmount) {
+		this.payAmount = payAmount;
+	}
+
+	public int getMyPoint() {
+		return myPoint;
+	}
+	public void setMyPoint(int myPoint) {
+		this.myPoint = myPoint;
+	}
+	
+	/**
+	 * 결제 정보 출력
+	 */
+	public void processPay() {
+		System.out.println(" ===== 포인트 결제 정보 =====");
+		System.out.println("- 결제 금액:" + payAmount);
+		System.out.println("- 내 포인트 잔액:" + myPoint);
+		
+		// 결제 금액 < 포인트 잔액 => "결제 성공" 출력
+		// 결제 금액 > 포인트 잔액 = > "결제 실패(잔액 부족)
+		
+		if (payAmount >= myPoint) {
+			System.out.println("결제 성공");
+		} else {
+			System.out.println("포인트 잔액이 부족합니다.");
+		}
+		/*
+		  System.out.println((payAmount > myPoint? 
+		  				"포인트 잔액이 부족합니다." : "결제 성공"));
+		*/
+	}
+	
+
+}
